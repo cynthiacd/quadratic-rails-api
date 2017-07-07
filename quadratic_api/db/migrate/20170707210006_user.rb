6baee2e6752488c4c54cae89a01399e6bf292d60
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-07 14:43:54 -0700

    migrated the users db table

---
+++ quadratic_api/db/migrate/20170707210006_user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :user_name
+      t.integer :pattern_plus_plus
+      t.integer :pattern_minus_plus
+      t.integer :pattern_minus_minus
+      t.integer :pattern_plus_minus
+      t.integer :pattern_plus_dbl_sq
+      t.integer :pattern_minus_dbl_sq
+      t.integer :pattern_diff_sq
+      t.integer :pattern_primes
+      t.integer :pattern_ones_and_zeros
+      t.integer :pattern_gcf
+
+      t.timestamps
+    end
+  end
+end
