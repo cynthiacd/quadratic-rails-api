41b92880f23d1bbb8adeaf58d7e1a3395521c340
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-07 14:46:15 -0700

    fixed bug with model/migration with users

--- quadratic_api/db/migrate/20170707214510_create_users.rb
+++ quadratic_api/db/migrate/20170707214510_create_users.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Migration[5.0]
+class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       t.string :user_name
@@ -12,7 +12,7 @@ class User < ActiveRecord::Migration[5.0]
       t.integer :pattern_primes
       t.integer :pattern_ones_and_zeros
       t.integer :pattern_gcf
-
+      
       t.timestamps
     end
   end
