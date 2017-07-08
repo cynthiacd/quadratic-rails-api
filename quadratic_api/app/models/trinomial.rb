bbb9dc17e30e4eb301557760edd7efd14092967b
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-08 11:01:06 -0700

    added api response will include the pattern and a string in general form

--- quadratic_api/app/models/trinomial.rb
+++ quadratic_api/app/models/trinomial.rb
@@ -10,7 +10,12 @@ class Trinomial
     b = @root1 + @root2
     c = @root1 * @root2
     # return a hash?
-    return { a: 1, b: b, c: c }
+    return {
+            pattern: "plus_plus",
+            general_form: "x^2 + #{b}x + #{c}",
+            a: 1,
+            b: b,
+            c: c }
     # or could return an array?
     # return [1, b, c]
   end
