e27f9fa7d20c01f53c2cdffe60d6f66e03fed200
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-07-10 14:46:22 -0700

    refractored update mastery_mastery_level method in user to grade any type of problem

--- quadratic_api/app/models/user.rb
+++ quadratic_api/app/models/user.rb
@@ -2,12 +2,8 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
 
   def update_mastery_levels(problem_info)
-    # change grade to score or points
-    score = problem_info["score"]
     pattern = problem_info["pattern"]
-    if pattern == "plus_plus"
-      self.mastery_plus_plus += score
-      self.save
-    end
+    self[pattern] += problem_info["score"]
+    self.save
   end
 end
